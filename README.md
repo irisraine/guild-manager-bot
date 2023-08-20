aad64d3135268acb0228b7a5bd619debfd0677bc
Iris-Raine <iris.raine.darkest.star@gmail.com>
2023-08-20 23:35:58 +0000

    add moderation functions against nsfw and raids

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 
 ## Description
 
-This Discord bot is designed to automatically create a discussion thread in the current channel whenever a user posts 
+This Discord bot is designed to automatically create a discussion threads in the current channel whenever a user posts 
 an image. The bot will use the image as the initial message content for the thread and will include a welcome message 
 to encourage users to start a discussion.
+Also this bot provides a moderation functions. It designed to automatically detect and delete NSFW images, temporarily 
+block users who post them, as well as identify and remove spam messages during raids, while blocking the spammers.
+
+The bot sends the uploaded images to the Microsoft Content Moderator service, which analyzes the images and returns a confidence 
+score indicating the likelihood of the image containing NSFW content. The bot then compares this score to a predefined 
+threshold to determine whether the image should be considered NSFW.
+By utilizing the Content Moderator service, the bot can accurately detect and moderate NSFW images in real-time, ensuring 
+a safe and appropriate environment for all users.
 
 This bot does not have any commands, as it is designed to run automatically in the background.
 
@@ -12,12 +20,15 @@ This bot does not have any commands, as it is designed to run automatically in t
 
 If you want to use this bot locally, please make sure to specify the necessary environment variables by creating a .env file 
 in the project directory. This file should contain TOKEN and ALLOWED_CHANNELS environment variables and their corresponding values.
+Finally, you need an API key of Microsoft Content Moderator service.
 Here is an example:
 ```
 # Bot token
 TOKEN='your-discord-bot-token-here'
 # Allowed channels
 ALLOWED_CHANNELS=1111111111, 2222222222
+# API key of Microsoft Content Moderator service
+NSFW_CONTENT_MODERATOR_API_KEY='your-api-key-here'
 ```
 You can intentionally leave blank the ALLOWED_CHANNELS variable, so bot will have permission to create threads in any public channel.
 
--- engine/bot.py
+++ engine/bot.py
@@ -1,55 +1,157 @@
+from collections import deque
+from datetime import datetime, timedelta
 import nextcord
 from nextcord.ext import commands
 from dotenv import load_dotenv
 import requests
 import os
 import re
+from engine.nsfw_checker import is_image_nsfw
 
 load_dotenv()
 ALLOWED_CHANNELS = os.environ['ALLOWED_CHANNELS']
 
-intents = nextcord.Intents.default()
-intents.message_content = True
-intents.messages = True
+intents = nextcord.Intents.all()
 client = commands.Bot(command_prefix='!', intents=intents)
 
 COMMENTS_THREAD_NAME = "💬 Оставить комментарии"
 GREETING_BOT_MESSAGE = "Создана ветка обсуждения"
+MUTE_HEADER_MESSAGE = '❌ Здравствуйте, вам бан! ❌'
+MUTE_REASON_SPAM = "спамил картинками"
+MUTE_REASON_NSFW = "постил непотребства"
+MUTE_DESCRIPTION_MESSAGE = 'Теперь он улетает в мут, хорошенько подумать о своем поведении!'
+TIMEOUT_DURATION = 9000
+MAX_IMAGES = 10
+TIME_LIMIT = 60
+
+current_time = 0
+
+user_message_statistics = {}
+active_timeout = {}
 
 
-def has_attached_image(message):
+async def has_attached_image(message):
     if message.attachments:
         attachment = message.attachments[0]
         return attachment.content_type.startswith('image/')
-    else:
-        pattern = re.compile(r'(http|https)://\S+')
-        if pattern.match(message.content):
-            try:
-                response = requests.head(message.content)
-                if response.status_code == 200:
-                    return 'image' in response.headers.get('content-type')
-            except requests.exceptions.RequestException:
-                return False
+
+
+async def get_image_links(message):
+    url_pattern = re.compile(r'(http|https)://\S+')
+    message_content_as_list = message.content.split("\n")
+    urls = list(filter(lambda item: url_pattern.match(item), message_content_as_list))
+    image_urls = []
+    for url in urls:
+        try:
+            response = requests.head(url)
+            if response.status_code == 200 and 'image' in response.headers.get('content-type'):
+                image_urls.append(url)
+        except requests.exceptions.RequestException:
+            continue
+    return image_urls if image_urls else []
+
+
+async def mute_user(message, reason):
+    try:
+        await message.author.timeout(timedelta(
+            seconds=TIMEOUT_DURATION),
+            reason=reason
+        )
+        print(f'User {message.author} has been muted.')
+    except nextcord.errors.Forbidden:
+        print(f'Privileged users cannot be blocked by this bot!')
 
 
 @client.event
 async def on_message(message):
-    if ALLOWED_CHANNELS and str(message.channel.id) not in ALLOWED_CHANNELS:
-        return
     if message.author.bot:
         return
 
-    if has_attached_image(message):
-        try:
-            thread = await message.create_thread(
-                name=COMMENTS_THREAD_NAME,
-                auto_archive_duration=60
+    message_has_attached_images = await has_attached_image(message)
+    message_image_links = await get_image_links(message)
+
+    is_nsfw = False
+
+    if message_has_attached_images or message_image_links:
+        user_id = message.author.id
+
+        if user_id not in user_message_statistics:
+            user_message_statistics[user_id] = deque(maxlen=MAX_IMAGES)
+        user_message_statistics[user_id].append(message.created_at.timestamp())
+
+        if user_id in user_message_statistics and len(user_message_statistics[user_id]) == MAX_IMAGES:
+            oldest_message_time = user_message_statistics[user_id].popleft()
+            global current_time
+            current_time = message.created_at.timestamp()
+            if current_time - oldest_message_time <= TIME_LIMIT:
+                active_timeout[user_id] = {'channel': message.channel.id,
+                                            'reason': MUTE_REASON_SPAM
+                                           }
+                await mute_user(message, MUTE_REASON_SPAM)
+                user_message_statistics.pop(user_id, None)
+                return
+
+        message_attachment_images_url = [attachment.url for attachment in message.attachments]
+        message_all_images_urls = message_attachment_images_url + message_image_links
+
+        for image_url in message_all_images_urls:
+            if await is_image_nsfw(image_url):
+                is_nsfw = True
+                active_timeout[user_id] = {'channel': message.channel.id,
+                                            'reason': MUTE_REASON_NSFW
+                                           }
+                await mute_user(message, MUTE_REASON_NSFW)
+                try:
+                    await message.delete()
+                except nextcord.errors.NotFound:
+                    print(f'Message not found or already deleted')
+
+                break
+
+        thread_create_permission = True
+        if ALLOWED_CHANNELS:
+            if str(message.channel.id) not in ALLOWED_CHANNELS:
+                thread_create_permission = False
+
+        if thread_create_permission and not is_nsfw:
+            try:
+                thread = await message.create_thread(
+                    name=COMMENTS_THREAD_NAME,
+                    auto_archive_duration=60
+                )
+                await thread.send(GREETING_BOT_MESSAGE)
+            except nextcord.errors.HTTPException as e:
+                print(f'An error occurred while creating a thread: {e}')
+            except Exception as e:
+                print(f'An unexpected error occurred: {e}')
+
+    await client.process_commands(message)
+
+
+@client.event
+async def on_member_update(before, after):
+    if not before.communication_disabled_until and after.communication_disabled_until:
+        if after.id in active_timeout.keys():
+            channel_id = active_timeout[after.id]['channel']
+            channel = client.get_channel(channel_id)
+            reason_for_muting = active_timeout[after.id]['reason']
+
+            ban_info = nextcord.Embed(
+                title=MUTE_HEADER_MESSAGE,
+                description=f'Абоба {after.mention} {reason_for_muting}. {MUTE_DESCRIPTION_MESSAGE}',
+                colour=nextcord.Colour.from_rgb(255, 0, 0)
             )
-            await thread.send(GREETING_BOT_MESSAGE)
-        except nextcord.errors.HTTPException as e:
-            print(f'An error occurred while creating a thread: {e}')
-        except Exception as e:
-            print(f'An unexpected error occurred: {e}')
+            await channel.send(embed=ban_info)
+            del active_timeout[after.id]
+
+            if reason_for_muting == MUTE_REASON_SPAM:
+                spam_starting_time = datetime.fromtimestamp(current_time - TIME_LIMIT)
+                async for message in channel.history(limit=100, after=spam_starting_time):
+                    if message.author == after:
+                        try:
+                            await message.delete()
+                        except nextcord.errors.NotFound:
+                            print(f'Message not found or already deleted')
 
 
 @client.event
