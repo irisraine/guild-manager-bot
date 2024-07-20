642633a6bc7844446b97984e789a7ede125f1fb8
Iris-Raine <iris.raine.darkest.star@gmail.com>
2024-07-20 18:37:40 +0000

    add replace some in-code text literals with constans

--- engine/cogs/solo_manager.py
+++ engine/cogs/solo_manager.py
@@ -3,6 +3,12 @@ from nextcord.ext import commands
 import logging
 import engine.config as config
 
+ERROR_HEADER = "Ошибка!"
+SOLO_MANAGER_USAGE_RESTRICTION = ("Выдавать гражданство славного города Подфайловска "
+                                  "могут только админы, маршалы и предводители банд!")
+ROLE_SET = "✅ Роль выдана"
+ROLE_REMOVED = "❌ Роль снята"
+
 
 class SoloToggler(commands.Cog):
     def __init__(self, client):
@@ -13,9 +19,8 @@ class SoloToggler(commands.Cog):
         if not (ctx.author.guild_permissions.administrator or
                 any(role.id in [config.MODERATOR_ROLE_ID] + config.GROUP_LEADERS_ROLES for role in ctx.author.roles)):
             await ctx.send(embed=nextcord.Embed(
-                title="Ошибка!",
-                description="Выдавать гражданство славного города Подфайловска "
-                            "могут только админы, маршалы и предводители банд!",
+                title=ERROR_HEADER,
+                description=SOLO_MANAGER_USAGE_RESTRICTION,
                 color=nextcord.Color.red()
             ))
             return
@@ -24,7 +29,7 @@ class SoloToggler(commands.Cog):
         role = nextcord.utils.get(ctx.guild.roles, id=config.SOLO_SESSION_ROLE)
         if not member:
             await ctx.send(embed=nextcord.Embed(
-                title="Ошибка!",
+                title=ERROR_HEADER,
                 description=f"Пользователь с именем {username} не присутствует на сервере.",
                 color=nextcord.Color.red()
             ))
@@ -33,7 +38,7 @@ class SoloToggler(commands.Cog):
         if role in member.roles:
             await member.remove_roles(role)
             await ctx.send(embed=nextcord.Embed(
-                title="❌ Роль снята",
+                title=ROLE_REMOVED,
                 description=f"Ковбой {member.mention} лишился роли {role.mention}, а вместе с ней и гражданства "
                             f"города Подфайловска, и был вынужден покинуть его.\n\n "
                             f"*Роль снял {ctx.author.mention}*",
@@ -44,7 +49,7 @@ class SoloToggler(commands.Cog):
         else:
             await member.add_roles(role)
             await ctx.send(embed=nextcord.Embed(
-                title="✅ Роль выдана",
+                title=ROLE_SET,
                 description=f"Ковбой {member.mention} получает роль {role.mention} "
                             f"и триумфально въезжает в город Подфайловск, становясь его полноправным гражданином!\n\n "
                             f"*Роль выдал {ctx.author.mention}*",
