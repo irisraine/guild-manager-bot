f8e7eaecdc82d1ae7ac81da6db16c128aab77253
Iris-Raine <iris.raine.darkest.star@gmail.com>
2024-10-07 19:05:17 +0000

    move loading token env to config

--- engine/run.py
+++ engine/run.py
@@ -1,17 +1,13 @@
 from engine.bot import client
 from engine.logger import init_logger
 from engine.utils import load_cogs
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+import engine.config as config
 
 
 def run_discord_bot():
     init_logger()
     load_cogs(client)
-    client.run(DISCORD_BOT_TOKEN)
+    client.run(config.DISCORD_BOT_TOKEN)
 
 
 if __name__ == "__main__":
